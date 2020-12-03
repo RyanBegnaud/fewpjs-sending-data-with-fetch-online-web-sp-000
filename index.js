cdc9cc5d82666435a46ba485a96217ffc83b53e0
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-03 23:12:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-let configObj = {
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
-
 
 function submitData(name,email) {
   let configObj = {
