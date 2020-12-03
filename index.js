86c62a972029cfbddaf26de4242027ee0b335078
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-03 22:58:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+let configObj = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
+}
+
+
 function submitData(name,email) {
   fetch("http://localhost:3000/users")
+  
 }
