c5103ad39c82d3e8eff9161d326728ee57ce2a00
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-03 22:29:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-configurationObject = {
+fetch("http://localhost:3000/dogs", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json"
   },
-  body: /* Your data goes here */
-};
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  })
+});
